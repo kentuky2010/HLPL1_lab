11120c5c297b107374552a89a7f83e45cefe3f30
kentuky2010 <71006617+kentuky2010@users.noreply.github.com>
2021-05-12 00:58:05 +0200

    Update Drill 9.4.2.cpp

--- Drill 9.4.2.cpp
+++ Drill 9.4.2.cpp
@@ -5,16 +5,9 @@ struct Date
 	int y,m,d;
 	Date (int yy, int mm, int dd)
 	{
-		if (m < 1 || m > 12) error("Invalid month");
-		if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-		{
-			if (d < 1 || d > 31) error("Invalid date");
-		}
-		else if (m == 2)
-		{
-			if (d < 1 || d > 29) error("Invalid date");
-		}
-		else if (d < 1 || d > 30) error("Invalid date");
+		if (yy < 0) error("Invalid year");
+		if (mm < 1 || mm > 12) error("Invalid month");
+		if (dd < 1 || dd > 31) error("Invalid date");
 		y = yy;
 		m = mm;
 		d = dd;
@@ -22,14 +15,7 @@ struct Date
 
 	void add_day(int n)
 	{
-		int max;
-		d += n;
-		if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-			max = 31;
-		else if (m == 2)
-			max = 29;
-		else max = 30;
-		while (d > m)
+		while (d > 31)
 		{
 			d -= 31;
 			m++;
@@ -44,22 +30,19 @@ struct Date
 
 ostream& operator<<(ostream& os, const Date& d)
 {
-	return os << '(' << d.y << ',' << d.m << ',' << d.d << ')';
-}
-
-void f()
-{
-	Date today(1978, 6, 25);
-	cout << today << "\n";
-	cout << today << "\n";
+	return os << d.y << "." << d.m << "." << d.d;
 }
 
 int main()
-try{
-	f();
+try {
+
+	Date today = { 1978, 6, 25 };
+	cout << today << endl;
+	Date tomorrow = today;
+	tomorrow.add_day(1);
+	cout << tomorrow << endl;
 	return 0;
 }
-
 catch (exception& e) {
 	cerr << e.what() << endl;
 	return 1;

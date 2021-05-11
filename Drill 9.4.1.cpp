f1e6da4930ef1fb348a308e831354844941f197b
kentuky2010 <71006617+kentuky2010@users.noreply.github.com>
2021-05-12 01:00:54 +0200

    Update Drill 9.4.1.cpp

--- Drill 9.4.1.cpp
+++ Drill 9.4.1.cpp
@@ -2,24 +2,14 @@
 // Drill 9.4.1
 struct Date
 {
-	int y; 
-	int m; 
-	int d; 
+	int y, m, d;
 };
 
 void init_day(Date& dd, int y, int m, int d)
 {
+	if (y < 0) error("Invalid year");
 	if (m < 1 || m > 12) error("Invalid month");
-	if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-	{
-		if (d < 1 || d > 31) error("Invalid date");
-	}
-	else if (m == 2)
-	{
-		if (d < 1 || d > 29) error("Invalid date");
-	}
-	else if (d < 1 || d > 30) error("Invalid date");
-	
+	if (d < 1 || d > 31) error("Invalid date");
 
 	dd.y = y;
 	dd.m = m;
@@ -30,11 +20,6 @@ void add_day(Date& dd, int n)
 {
 	int m;
 	dd.d += n;
-	if (dd.m == 1 || dd.m == 3 || dd.m == 5 || dd.m == 7 || dd.m == 8 || dd.m == 10 || dd.m == 12)
-		m = 31;
-	else if (dd.m == 2)
-		m = 29;
-	else m = 30;
 	while (dd.d > m) 
 	{
 		dd.d -= 31;
@@ -49,7 +34,7 @@ void add_day(Date& dd, int n)
 
 ostream& operator<<(ostream& os, const Date& d)
 {
-	return os << '(' << d.y << ',' << d.m << ',' << d.d << ')';
+	return os << d.y << "." << d.m << "." << d.d;
 }
 
 void f()
@@ -58,7 +43,7 @@ void f()
 	init_day(today, 1978, 6, 25);
 	cout << today << "\n";
 	Date tomorrow = today;
-	add_day(tomorrow, 1);
+	add_day(tomorrow, 365);
 	cout << tomorrow << "\n";
 }
 int main()
